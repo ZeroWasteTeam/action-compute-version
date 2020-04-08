f4991a93a601d6f307d2d7515074880b2e668f07
Nachiappan <nachiapan@gmail.com>
2020-04-08 15:49:51 +0530

    Added more checks

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ if( ! fs.existsSync(fileName)) {
 try {
   var version = fs.readFileSync(fileName, 'utf8');
   version = version.trim();
-  var versionRegex = /^\d+\.\d+$/
-  if(!version.match(versionRegex)) throw new Error("The "+version+" is not of the format MAJOR.MINOR");
+  if(!version.match(/^\d+\.\d+$/)) throw new Error("The "+version+" is not of the format MAJOR.MINOR");
+  if (version == '0.0') throw new Error('0.0 is not a valid version. Either major version or minor version has to be non zero');
+  if (version.match(/^0\d+\./)) throw new Error("Major version can not be prefixed with 0");
+  if (version.match(/\.0\d+$/)) throw new Error("Minor version can not be prefixed with 0");
   console.log(">"+version+"<");	
 } catch (err) {
   console.error(err)
