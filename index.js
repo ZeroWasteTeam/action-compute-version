eb48a38d7e728268aa752a20bbf1f129af983afd
Nachiappan <nachiapan@gmail.com>
2020-04-08 14:19:30 +0530

    reading a file

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+
+const fs = require('fs')
+
 console.log("Hello from action");
+
+
+try {
+  const data = fs.readFileSync('version.txt', 'utf8')
+  console.log(data)
+} catch (err) {
+  console.error(err)
+}
