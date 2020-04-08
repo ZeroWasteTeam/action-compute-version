195f2d1b2929e2ef5e2069c734abbb1263244d9d
Nachiappan <nachiapan@gmail.com>
2020-04-08 17:07:27 +0530

    Update the action

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-
 const fs = require('fs');
 const Regex = require('regex');
 const core = require('@actions/core');
 
 const fileName = core.getInput('version-file');
 
-if( ! fs.existsSync(fileName)) {
-	throw new Error('The file '+fileName+ ' does not exists')
-}
 
 try {
+  if( ! fs.existsSync(fileName)) throw new Error('The file '+fileName+ ' does not exists')
   var version = fs.readFileSync(fileName, 'utf8');
   version = version.trim();
   if(!version.match(/^\d+\.\d+$/)) throw new Error("The "+version+" is not of the format MAJOR.MINOR");
   if (version == '0.0') throw new Error('0.0 is not a valid version. Either major version or minor version has to be non zero');
   if (version.match(/^0\d+\./)) throw new Error("Major version can not be prefixed with 0");
   if (version.match(/\.0\d+$/)) throw new Error("Minor version can not be prefixed with 0");
+  console.
   core.setOutput("version",version);	
 } catch (err) {
   console.error(err)
+  core.setError(err.Message);
 }
