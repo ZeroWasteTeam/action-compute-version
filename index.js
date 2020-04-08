1e448a611b10d75018287a7a6eb078175659dfcf
Nachiappan <nachiapan@gmail.com>
2020-04-08 17:49:12 +0530

    Done

--- index.js
+++ index.js
@@ -13,20 +13,22 @@ exec('pwd', (err, stdout, stderr) => {
   } else {
    console.log(`stdout: ${stdout}`);
    console.log(`stderr: ${stderr}`);
+   
+   
+   try {
+     if( ! fs.existsSync(fileName)) throw new Error('The file '+fileName+ ' does not exists')
+     var version = fs.readFileSync(fileName, 'utf8');
+     version = version.trim();
+     if(!version.match(/^\d+\.\d+$/)) throw new Error("The "+version+" is not of the format MAJOR.MINOR");
+     if (version == '0.0') throw new Error('0.0 is not a valid version. Either major version or minor version has to be non zero');
+     if (version.match(/^0\d+\./)) throw new Error("Major version can not be prefixed with 0");
+     if (version.match(/\.0\d+$/)) throw new Error("Minor version can not be prefixed with 0");
+     console.log("The version is "+version);
+     core.setOutput("version",version);	
+   } catch (err) {
+     console.error(err)
+     core.setFailed(err.message);
+   }
   }
 });
 
-try {
-  if( ! fs.existsSync(fileName)) throw new Error('The file '+fileName+ ' does not exists')
-  var version = fs.readFileSync(fileName, 'utf8');
-  version = version.trim();
-  if(!version.match(/^\d+\.\d+$/)) throw new Error("The "+version+" is not of the format MAJOR.MINOR");
-  if (version == '0.0') throw new Error('0.0 is not a valid version. Either major version or minor version has to be non zero');
-  if (version.match(/^0\d+\./)) throw new Error("Major version can not be prefixed with 0");
-  if (version.match(/\.0\d+$/)) throw new Error("Minor version can not be prefixed with 0");
-  console.log("The version is "+version);
-  core.setOutput("version",version);	
-} catch (err) {
-  console.error(err)
-  core.setFailed(err.message);
-}
