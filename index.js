3f5cb166929ea9ba10ef5c67c4e73d349dbef5e0
Nachiappan <nachiapan@gmail.com>
2020-04-08 15:11:22 +0530

    Cleaned up

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 
-const fs = require('file-system')
+const fs = require('fs')
 
 console.log("Hello from action");
 
+var fileName = 'version.txt'
+
+if( ! fs.existsSync(fileName)) {
+	throw new Error('The file '+fileName+ ' does not exists')
+}
 
 try {
-  const data = fs.readFileSync('version.txt', 'utf8')
+  const data = fs.readFileSync(fileName, 'utf8')
   console.log(data)
 } catch (err) {
   console.error(err)
