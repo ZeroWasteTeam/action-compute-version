6c2d1e7d702726c1968de417bedafb75068385c8
Nachiappan <nachiapan@gmail.com>
2020-04-14 18:45:47 +0530

    Modified code

--- verify-pr-version/dist/index.js
+++ verify-pr-version/dist/index.js
@@ -525,7 +525,7 @@ core.debug(`The origin base branch is  ${originBaseBranch}`);
 if ( baseBranch == "" ) throw new Error ( "The base branch is not set");
 if ( mergedSha == "" ) throw new Error ( "The merged ref is not set");
 
-async function executeBashCommand(command) {	
+async function ExecuteBashCommand(command) {	
     const res = await exec(command);
     const { stdout, stderr } = res;
     let result = stdout.replace(/\n/g,'').replace(/\r/g,'');
@@ -533,43 +533,43 @@ async function executeBashCommand(command) {
 	return result;
 }
 
-async function isVersionModified() {
+async function IsVersionModified() {
 	let command = `git diff --name-only "${originBaseBranch}..${mergedSha}" ${fileName} | wc -l`;
-	let result = await executeBashCommand(command);s
+	let result = await ExecuteBashCommand(command);
 	return result != 0;
 }
 
 
-async function isNonVersionFilesModified(isVersionModified) {
+async function IsNonVersionFilesModified(isVersionModified) {
 	let command = `git diff --name-only "${baseBranch}..${mergedSha}"  | wc -l`;
-	var result = await executeBashCommand(command);
+	var result = await ExecuteBashCommand(command);
 	let numberOfNonVersionFilesModified =  isVersionModified? result - 1 : result;
 	return (numberOfNonVersionFilesModified != 0);
 }
 
-async function verifyVersionChangeInPullRequest() {
+async function VerifyVersionChangeInPullRequest() {
 	core.debug(`Verifying version change`);
 	if(baseBranch == defaultBranchName) {
 		core.debug(`Base branch and default branch name is same: ${baseBranch}`);
-		if(await isVersionModified()){
+		if(await IsVersionModified()){
 			core.debug(`The version file is modified`);
-			if(await isNonVersionFilesModified(isVersionModified = true)){
+			if(await IsNonVersionFilesModified(isVersionModified = true)){
 				core.debug(`Files other than version file are also modified`);
 				throw new Error("When modifying version file. No other file should be modified");
 			} 
-			let baseVersion = await getBaseVersion(baseBranch);
+			let baseVersion = await GetBaseVersion(baseBranch);
 			baseVersion = baseVersion.trim();
-			let modifiedVersion = await getBaseVersion(mergedSha);
+			let modifiedVersion = await GetBaseVersion(mergedSha);
 			modifiedVersion = modifiedVersion.trim();
 			
 			if(modifiedVersion == "") {
 				core.debug("Could not read the version information. This happens when version file : ${fileName} is not present");
 				throw new Error(`Could not read the version from ${fileName}`);
 			}
-			validateBaseVersion(modifiedVersion);
+			ValidateBaseVersion(modifiedVersion);
 			let isValidBaseVersion = false;
 			try{
-				validateBaseVersion(baseVersion);
+				ValidateBaseVersion(baseVersion);
 				isValidBaseVersion = true;
 			}catch(err){
 				
@@ -586,15 +586,14 @@ async function verifyVersionChangeInPullRequest() {
 			return;
 		}
 	} else {
-		if(await isVersionModified()){
+		if(await IsVersionModified()){
 			core.debug(`Version file is modified`);
 			throw new Error("Version file modification is not permitted in ");		
 		} 
 	}	
 }
 
-function validateBaseVersion(version) {
-	
+function ValidateBaseVersion(version) {	
 	if(!version.match(/^\d+\.\d+$/)) throw new Error(`The version: ${version}" is not of the format MAJOR.MINOR`);
 	if (version == '0.0') throw new Error('0.0 is not a valid version. Either major version or minor version has to be non zero');
 	if (version.match(/^0\d+\./)) throw new Error("Major version can not be prefixed with 0");
@@ -602,12 +601,12 @@ function validateBaseVersion(version) {
 	return version;
 }
 
-async function getBaseVersion(ref){
+async function GetBaseVersion(ref){
 	let command = 'git show "${ref}:version.txt"'
-	return await executeBashCommand(command);
+	return await ExecuteBashCommand(command);
 }
 
-verifyVersionChangeInPullRequest()
+VerifyVersionChangeInPullRequest()
 .then( x =>  {  console.log(`There is no problem in versioning!!!`); core.setFailed("For testing"); })
 .catch( x => {console.error(x); core.setFailed("For testing"); });
 
